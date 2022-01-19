716c498a5177c8a11a5789f960427e262fc63f12
maziyar <maziroadtodivine@gmail.com>
2022-01-19 17:08:20 +0330

    Initial commit. Work on custom input component and global styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
+<!-- eslint-disable --> 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="list-wrapper">
+    <form @submit.prevent="formSubmitted" class="list__form">
+      <p class="list__title">فرم استخدام</p>
+      <BaseInput
+        v-model="firstName"
+        :label="formLabelAndPlaceholder.firstName"
+        type="text"
+        :placeHolder="formLabelAndPlaceholder.firstName"
+      />
+
+      <BaseInput
+        v-model="lastName"
+        :label="formLabelAndPlaceholder.lastName"
+        type="text"
+        :placeHolder="formLabelAndPlaceholder.lastName"
+      />
+
+      <BaseInput
+        v-model="age"
+        label="سن"
+        type="number"
+      />
+
+      <button class="list__button">تایید</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+/* eslint-disable-next-line */
+/* eslint-disable */
+import { ref } from "@vue/reactivity";
+import BaseInput from "./components/BaseInput.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    BaseInput,
+  },
+  setup() {
+    const firstName = ref(null);
+    const lastName = ref(null);
+    const age = ref('')
+    const formLabelAndPlaceholder = ref({
+      firstName: "نام",
+      lastName: "نام خانوادگی",
+    });
+
+    const formSubmitted = () => {
+      console.log({ firstName: firstName.value, lastName: lastName.value , userAge : age.value });
+    };
+
+    return {
+      firstName,
+      lastName,
+      formLabelAndPlaceholder,
+      age,
+      formSubmitted,
+    };
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import "./assets/styles/global.css";
+@font-face {
+  font-family: "IranSans";
+  src: url("./assets/fonts/iransans.ttf");
+}
+
+.list-wrapper {
+  width: 100%;
+  height: 100vh;
+  background-color: #f7f6f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.list__title {
+  color: #222;
+  font-size: 30px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 10px;
+}
+
+.list__form {
+  width: 500px;
+  height: 500px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  padding: 15px 10px;
+  border-radius: 10px;
+  box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.07);
+}
+.list__button {
+  margin: 20px 20px 0;
+  height: 30px;
+  border-radius: 5px;
+  color: #000;
+  background: #c69e4e;
 }
 </style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,48 @@
+<!--  eslint-disable  -->
+<!--  eslint-disable-next-line  -->
+<template>
+  <div class="input-wrapper">
+    <label class="input__label">{{ label }}</label>
+    <input
+      class="input__field"
+      :value="modelValue"
+      :type="type"
+      :placeholder="placeHolder"
+      @input="(e) => $emit('update:modelValue', e.target.value)"
+    />
+  </div>
+</template>
+<script>
+/* eslint-disable-next-line */
+/* eslint-disable */
+export default {
+  name: "CustomInput",
+  props: {
+    modelValue: [Number, String],
+    label: {
+      type: String,
+      default: "این یک لیبل میباشد",
+    },
+    type: String,
+    placeHolder : String
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px;
+  .input__label{
+     font-size: 18px;
+     color: #555;
+     margin-bottom: 5px;
+  }
+  .input__field{
+     border-radius: 3px;
+     height: 30px;
+     padding: 4px 10px 4px 20px;
+  }
+}
+</style>
